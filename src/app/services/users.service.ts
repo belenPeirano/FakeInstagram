b1b22601c321fae3bf731edb92abca4631c297c3
PVmendez <valentino.mendez.rey@gmail.com>
2023-05-31 19:08:42 -0300

    fix: improve styles search component

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -34,32 +34,6 @@ export class UsersService {
     },
     {
       id: '1',
-      username: '@gigliotti09',
-      userFullName: 'Emmanuel Gigliotti',
-      userPhotoPath: '',
-      postsList: [
-        {
-          id: '0',
-          photoPath:
-            'https://storage.googleapis.com/tenfield-storage/2022/10/35613d93-gigliotti-festejo-gol-vs-cerrito-apertura.jpg',
-          tag: 'argentinos',
-        },
-        {
-          id: '1',
-          photoPath:
-            'https://el-pais.brightspotcdn.com/dims4/default/d03f0fb/2147483647/strip/true/crop/1551x1066+25+0/resize/1440x990!/quality/90/?url=https%3A%2F%2Fel-pais-uruguay-production-web.s3.amazonaws.com%2Fbrightspot%2Fuploads%2F2022%2F08%2F19%2F62ffe8389344e.jpeg',
-          tag: 'argentinos',
-        },
-        {
-          id: '2',
-          photoPath:
-            'https://soydelrojo.com/wp-content/uploads/2022/05/51ab69c7099bdae20e6a2d2d1eb54e8f67a7b8a1.jfif_.jpg',
-          tag: 'argentinos',
-        },
-      ],
-    },
-    {
-      id: '2',
       username: '@frangundez10',
       userFullName: 'Franco Fagundez',
       userPhotoPath: '',
@@ -85,36 +59,62 @@ export class UsersService {
       ],
     },
     {
-      id: '3',
+      id: '2',
       username: '@didiMessi22',
       userFullName: 'Diego Zabala',
       userPhotoPath: '',
       postsList: [
-        { id: '0', photoPath: '', tag: 'uruguayos' },
-        { id: '1', photoPath: '', tag: 'uruguayos' },
-        { id: '2', photoPath: '', tag: 'uruguayos' },
+        { id: '0', photoPath: 'https://cdn.elobservador.com.uy/022022/1644411655549.jpg', tag: 'uruguayos' },
+        { id: '1', photoPath: 'https://a.espncdn.com/photo/2022/1102/r1085128_1181x665_16-9.jpg', tag: 'uruguayos' },
+        { id: '2', photoPath: 'https://a3.espncdn.com/combiner/i?img=%2Fphoto%2F2023%2F0519%2Fr1175246_1296x729_16%2D9.jpg', tag: 'uruguayos' },
+      ],
+    },
+    {
+      id: '3',
+      username: '@luissuarez09',
+      userFullName: 'Luis Suarez',
+      userPhotoPath: '',
+      postsList: [
+        { id: '0', photoPath: 'https://diariocambio.com.uy/wp-content/uploads/2023/01/suarez-2.jpg', tag: 'uruguayos' },
+        { id: '1', photoPath: 'https://www.clarin.com/img/2022/08/05/luis-suarez-y-su-clasico___VfGT9yWy2_2000x1500__1.jpg', tag: 'uruguayos' },
+        { id: '2', photoPath: 'https://imagenes.montevideo.com.uy/imgnoticias/202210/822307.jpeg', tag: 'uruguayos' },
       ],
     },
     {
       id: '4',
-      username: '@condesteban',
-      userFullName: 'Esteban Conde',
-      userPhotoPath: '',
-      postsList: [
-        { id: '0', photoPath: '', tag: 'uruguayos' },
-        { id: '1', photoPath: '', tag: 'uruguayos' },
-        { id: '2', photoPath: '', tag: 'uruguayos' },
-      ],
-    },
-    {
-      id: '5',
       username: '@chino20recoba',
       userFullName: 'Alvaro Recoba',
       userPhotoPath: '',
       postsList: [
-        { id: '0', photoPath: '', tag: 'uruguayos' },
-        { id: '1', photoPath: '', tag: 'uruguayos' },
-        { id: '2', photoPath: '', tag: 'uruguayos' },
+        { id: '0', photoPath: 'https://storage.googleapis.com/tenfield-storage/2020/12/80b137ab-recoba-alvaro.jpg', tag: 'uruguayos' },
+        { id: '1', photoPath: 'https://chinorecobawines.uy/images/slide_n03.jpg', tag: 'uruguayos' },
+        { id: '2', photoPath: 'https://www.tenfield.com.uy/wp-content/uploads/2014/11/Alvaro-Recoba.jpg', tag: 'uruguayos' },
+      ],
+    },
+    {
+      id: '5',
+      username: '@gigliotti09',
+      userFullName: 'Emmanuel Gigliotti',
+      userPhotoPath: '',
+      postsList: [
+        {
+          id: '0',
+          photoPath:
+            'https://storage.googleapis.com/tenfield-storage/2022/10/35613d93-gigliotti-festejo-gol-vs-cerrito-apertura.jpg',
+          tag: 'argentinos',
+        },
+        {
+          id: '1',
+          photoPath:
+            'https://el-pais.brightspotcdn.com/dims4/default/d03f0fb/2147483647/strip/true/crop/1551x1066+25+0/resize/1440x990!/quality/90/?url=https%3A%2F%2Fel-pais-uruguay-production-web.s3.amazonaws.com%2Fbrightspot%2Fuploads%2F2022%2F08%2F19%2F62ffe8389344e.jpeg',
+          tag: 'argentinos',
+        },
+        {
+          id: '2',
+          photoPath:
+            'https://soydelrojo.com/wp-content/uploads/2022/05/51ab69c7099bdae20e6a2d2d1eb54e8f67a7b8a1.jfif_.jpg',
+          tag: 'argentinos',
+        },
       ],
     },
     {
@@ -123,9 +123,9 @@ export class UsersService {
       userFullName: 'Sergio Rochet',
       userPhotoPath: '',
       postsList: [
-        { id: '0', photoPath: '', tag: 'uruguayos' },
-        { id: '1', photoPath: '', tag: 'uruguayos' },
-        { id: '2', photoPath: '', tag: 'uruguayos' },
+        { id: '0', photoPath: 'https://el-pais.brightspotcdn.com/dims4/default/cd97963/2147483647/strip/true/crop/1383x951+30+0/resize/1440x990!/quality/90/?url=https%3A%2F%2Fel-pais-uruguay-production-web.s3.amazonaws.com%2Fbrightspot%2Fuploads%2F2022%2F06%2F29%2F62bcd012dfc38.jpeg', tag: 'uruguayos' },
+        { id: '1', photoPath: 'https://mediospublicos.uy/wp-content/uploads/WhatsApp-Image-2023-02-08-at-3.20.42-PM.jpeg', tag: 'uruguayos' },
+        { id: '2', photoPath: 'https://grupormultimedio.com/wp-content/uploads/2023/01/Sergio-Rochet-5.jpg', tag: 'uruguayos' },
       ],
     },
     {
@@ -134,9 +134,9 @@ export class UsersService {
       userFullName: 'Diego Polenta',
       userPhotoPath: '',
       postsList: [
-        { id: '0', photoPath: '', tag: 'uruguayos' },
-        { id: '1', photoPath: '', tag: 'uruguayos' },
-        { id: '2', photoPath: '', tag: 'uruguayos' },
+        { id: '0', photoPath: 'https://el-pais.brightspotcdn.com/dims4/default/92f12a2/2147483647/strip/true/crop/561x386+8+0/resize/1440x990!/quality/90/?url=https%3A%2F%2Fel-pais-uruguay-production-web.s3.amazonaws.com%2Fbrightspot%2Fuploads%2F2021%2F08%2F27%2F61295de596db4.jpeg', tag: 'uruguayos' },
+        { id: '1', photoPath: 'https://el-pais.brightspotcdn.com/dims4/default/8cdc31d/2147483647/strip/true/crop/1434x986+57+0/resize/1440x990!/quality/90/?url=https%3A%2F%2Fel-pais-uruguay-production-web.s3.amazonaws.com%2Fbrightspot%2F12%2F71%2Fb581e60544e6810b805593ab2103%2Fdiego-polenta.JPG', tag: 'uruguayos' },
+        { id: '2', photoPath: 'https://imagenes.montevideo.com.uy/imgnoticias/201901/680342.jpg', tag: 'uruguayos' },
       ],
     },
     {
@@ -145,9 +145,9 @@ export class UsersService {
       userFullName: 'Fabian Noguera',
       userPhotoPath: '',
       postsList: [
-        { id: '0', photoPath: '', tag: 'argentinos' },
-        { id: '1', photoPath: '', tag: 'argentinos' },
-        { id: '2', photoPath: '', tag: 'argentinos' },
+        { id: '0', photoPath: 'https://a.espncdn.com/photo/2023/0301/r1138222_1296x729_16-9.jpg', tag: 'argentinos' },
+        { id: '1', photoPath: 'https://storage.googleapis.com/tenfield-storage/2023/04/ace0dede-noguera-festrejo-nuevo.jpg', tag: 'argentinos' },
+        { id: '2', photoPath: 'https://imagenes.montevideo.com.uy/imgnoticias/202304/_W933_80/840203.jpeg', tag: 'argentinos' },
       ],
     },
     {
